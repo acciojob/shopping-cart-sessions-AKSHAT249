d6c9a39ff9d8292b26cfcc9cb531268495501de4
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-12-21 18:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -46,7 +46,8 @@ products.forEach((product) => {
   
   // Add item to cart
   function addToCart(productId) {
-	  let item = products[productId-1];
+	  // let item = products[productId-1];
+	  let item = products.find(product => product.id === productId);
 	  cartItems.push(item);
 	  renderCart();
 	  sessionStorage.setItem("cart-items", JSON.stringify(cartItems))
@@ -77,42 +78,3 @@ products.forEach((product) => {
  
   
 
-
-// // This is the boilerplate code given for you
-// // You can modify this code
-// // Product data
-// const products = [
-//   { id: 1, name: "Product 1", price: 10 },
-//   { id: 2, name: "Product 2", price: 20 },
-//   { id: 3, name: "Product 3", price: 30 },
-//   { id: 4, name: "Product 4", price: 40 },
-//   { id: 5, name: "Product 5", price: 50 },
-// ];
-
-// // DOM elements
-// const productList = document.getElementById("product-list");
-
-// // Render product list
-// function renderProducts() {
-//   products.forEach((product) => {
-//     const li = document.createElement("li");
-//     li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
-//     productList.appendChild(li);
-//   });
-// }
-
-// // Render cart list
-// function renderCart() {}
-
-// // Add item to cart
-// function addToCart(productId) {}
-
-// // Remove item from cart
-// function removeFromCart(productId) {}
-
-// // Clear cart
-// function clearCart() {}
-
-// // Initial render
-// renderProducts();
-// renderCart();
